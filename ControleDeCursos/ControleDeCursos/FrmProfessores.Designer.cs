753c38abffdea12dd6187a77cbfc48758197e48a
Gustavo Barbosa <gustavomb1313@gmail.com>
2021-09-27 20:23:00 -0300

    Add files via upload

---
+++ ControleDeCursos/ControleDeCursos/FrmProfessores.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace ControleDeCursos
+{
+    partial class FrmProfessores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnomeProfessor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValorAula = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dtgProfessor = new System.Windows.Forms.DataGridView();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProfessor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtnomeProfessor
+            // 
+            this.txtnomeProfessor.Location = new System.Drawing.Point(116, 41);
+            this.txtnomeProfessor.Name = "txtnomeProfessor";
+            this.txtnomeProfessor.Size = new System.Drawing.Size(328, 20);
+            this.txtnomeProfessor.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nome Completo:";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(116, 6);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtcodigo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Código do Professor:";
+            // 
+            // txtValorAula
+            // 
+            this.txtValorAula.Location = new System.Drawing.Point(116, 79);
+            this.txtValorAula.Name = "txtValorAula";
+            this.txtValorAula.Size = new System.Drawing.Size(100, 20);
+            this.txtValorAula.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Valor da hora/aula:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(12, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 57);
+            this.panel1.TabIndex = 22;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(27, 13);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(90, 30);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "CADASTRAR ";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "ALTERAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(318, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "LISTAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(222, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "EXCLUIR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dtgProfessor
+            // 
+            this.dtgProfessor.BackgroundColor = System.Drawing.Color.White;
+            this.dtgProfessor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProfessor.Location = new System.Drawing.Point(12, 186);
+            this.dtgProfessor.Name = "dtgProfessor";
+            this.dtgProfessor.Size = new System.Drawing.Size(432, 150);
+            this.dtgProfessor.TabIndex = 23;
+            this.dtgProfessor.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProfessor_CellClick);
+            this.dtgProfessor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgProfessor_CellContentClick);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(345, 79);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Telefone:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // FrmProfessores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(455, 353);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtgProfessor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtValorAula);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnomeProfessor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmProfessores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Protótipo: PROFESSORES";
+            this.Load += new System.EventHandler(this.FrmProfessores_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProfessor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnomeProfessor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValorAula;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dtgProfessor;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label4;
+    }
+}
